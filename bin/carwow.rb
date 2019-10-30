b6a8531c59cde76982cd8204c724d57a474b8f6b
Jasbir Singh <jasbir_singh@live.co.uk>
2019-10-30 00:07:57 +0000

    Create initial skeleton classes for the commands

--- bin/carwow.rb
+++ bin/carwow.rb
@@ -5,3 +5,29 @@
 #   V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
 #   H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
 #   S - Show the contents of the current image
+
+class CreateImageCommand
+end
+
+class ClearImageCommand
+end
+
+class ColourPixelCommand
+end
+
+class DrawVerticalCommand
+end
+
+class DrawHorizontalCommand
+end
+
+class ShowImageCommand
+end
+
+class CommandExecutor
+  def initialize(command)
+  end
+
+  def execute!
+  end
+end
