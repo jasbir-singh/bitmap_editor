b1d9e17f8aad7764d48df89bd6cfe7afd71709ee
Jasbir Singh <jasbir_singh@live.co.uk>
2019-10-30 21:53:59 +0000

    Rename files

--- specs/bitmap_editor_spec.rb
+++ specs/bitmap_editor_spec.rb
@@ -1,6 +1,6 @@
 require 'rspec'
 require 'pry'
-require_relative '../bin/carwow'
+require_relative '../bin/bitmap_editor'
 
 RSpec.describe CommandExecutor do
   let(:image) { CommandExecutor.new('I', '5', '6').execute! }
