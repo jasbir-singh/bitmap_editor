0ffcb1dd5da9f3c82c3a032aa36da5468351ed75
Jasbir Singh <jasbir_singh@live.co.uk>
2019-10-30 23:54:06 +0000

    Implement a REPL session to run commands

--- bin/bitmap_editor.rb
+++ bin/bitmap_editor.rb
@@ -102,3 +102,18 @@ class CommandExecutor
     command.execute!(*args)
   end
 end
+
+class BitmapEditor
+  def run
+    loop do
+      print("> ")
+      input = gets.chomp
+      @bitmap = CommandExecutor.new(*input.split(' ')).execute!(@bitmap)
+      CommandExecutor.new('S').execute!(@bitmap)
+    end
+  end
+end
+
+if __FILE__ == $0
+  BitmapEditor.new.run
+end
