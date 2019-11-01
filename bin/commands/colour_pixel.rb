fd08c5cdf7f57987bc35763b9cf1d5616d1d4242
Jasbir Singh <jasbir_singh@live.co.uk>
2019-11-01 23:02:03 +0000

    Add initial specs for colour pixel command

--- bin/commands/colour_pixel.rb
+++ bin/commands/colour_pixel.rb
@@ -1,15 +1,17 @@
-class Commands::ColourPixel
-  attr_reader :row, :column, :colour
+module Commands
+  class ColourPixel
+    attr_reader :row, :column, :colour
 
-  def initialize(column, row, colour)
-    # TODO: Handle the case where rows, columns are out of bound
-    @row = row.to_i - 1
-    @column = column.to_i - 1
-    @colour = colour
-  end
+    def initialize(column, row, colour)
+      # TODO: Handle the case where rows, columns are out of bound
+      @row = row.to_i - 1
+      @column = column.to_i - 1
+      @colour = colour
+    end
 
-  def execute!(image)
-    image[row, column] = colour
-    image
+    def execute!(image)
+      image[row, column] = colour
+      image
+    end
   end
 end
---
+++ spec/commands/colour_pixel_spec.rb
@@ -0,0 +1,14 @@
+require 'matrix'
+require_relative '../../bin/commands/colour_pixel'
+
+RSpec.describe Commands::ColourPixel do
+  let(:image) { Matrix.zero(2) }
+
+  describe '#execute' do
+    subject { described_class.new(1, 1, 'W').execute!(image) }
+
+    it 'colours the right pixel', wip: true do
+      expect(subject[0,0]).to eq('W')
+    end
+  end
+end
