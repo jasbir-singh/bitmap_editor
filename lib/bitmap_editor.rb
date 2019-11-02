870b6f7c25e42d1f2ce2999c338966c31201360c
Jasbir Singh <jasbir_singh@live.co.uk>
2019-11-02 01:56:49 +0000

    Refactor logic

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -4,15 +4,14 @@ class BitmapEditor
   def run(file_path)
     repl unless file_path
 
-    return puts "File doesn't exist" unless File.exist?(file_path)
-
-    run_commands_from_file(file_path) if file_path
+    run_commands_from_file(file_path)
   end
 
   private
 
   def run_commands_from_file(file_path)
-    # TODO: Handle the case when the file doesn't exist
+    return puts "File doesn't exist" unless File.exist?(file_path)
+
     IO.foreach(file_path) do |line|
       @current_bitmap = CommandExecutor.new(*line_to_command(line)).execute!(@current_bitmap)
     end
@@ -27,6 +26,7 @@ class BitmapEditor
       print('> ')
       input = gets.chomp
       @current_bitmap = CommandExecutor.new(*line_to_command(input)).execute!(@current_bitmap)
+
       CommandExecutor.new('S').execute!(@current_bitmap)
     end
   end
