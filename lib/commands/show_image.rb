0f0a7542f192ad795d17193b8f2389491b4120b5
Jasbir Singh <jasbir_singh@live.co.uk>
2019-11-02 15:27:43 +0000

    Handle the no image scenario

--- lib/commands/show_image.rb
+++ lib/commands/show_image.rb
@@ -1,7 +1,15 @@
 module Commands
   class ShowImage
     def execute!(image)
+      return no_image unless image
+
       image.to_a.each { |row| puts row.join }
+    end
+
+    private
+
+    def no_image
+      puts 'There is no image'
     end
   end
 end
--- spec/commands/show_image_spec.rb
+++ spec/commands/show_image_spec.rb
@@ -2,11 +2,20 @@ require_relative '../../lib/bitmap'
 require_relative '../../lib/commands/show_image'
 
 RSpec.describe Commands::ShowImage do
-  let(:image) { Bitmap[[1, 2], [3, 4]] }
-
   describe '#execute!' do
-    it 'prints out image' do
-      expect { described_class.new.execute!(image) }.to output("12\n34\n").to_stdout
+    context 'with no image' do
+      let(:image) { }
+
+      it 'returns error message' do
+        expect { described_class.new.execute!(image) }.to output(/There is no image/).to_stdout
+      end
+    end
+
+    context 'with valid image' do
+      let(:image) { Bitmap[[1, 2], [3, 4]] }
+      it 'prints out image' do
+        expect { described_class.new.execute!(image) }.to output("12\n34\n").to_stdout
+      end
     end
   end
 end
