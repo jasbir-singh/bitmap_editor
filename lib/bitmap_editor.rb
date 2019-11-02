6d8c464c14196ba7f197a57632af771fa1a01559
Jasbir Singh <jasbir_singh@live.co.uk>
2019-11-02 01:32:01 +0000

    Minor refactor: extract method

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -14,15 +14,19 @@ class BitmapEditor
   def run_commands_from_file(file_path)
     # TODO: Handle the case when the file doesn't exist
     IO.foreach(file_path) do |line|
-      @bitmap = CommandExecutor.new(*line.split(' ')).execute!(@bitmap)
+      @bitmap = CommandExecutor.new(*line_to_command(line)).execute!(@bitmap)
     end
+  end
+
+  def line_to_command(line)
+    line.split(' ')
   end
 
   def repl
     loop do
       print('> ')
       input = gets.chomp
-      @bitmap = CommandExecutor.new(*input.split(' ')).execute!(@bitmap)
+      @bitmap = CommandExecutor.new(*line_to_command(input)).execute!(@bitmap)
       CommandExecutor.new('S').execute!(@bitmap)
     end
   end
